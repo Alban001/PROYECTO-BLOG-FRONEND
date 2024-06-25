5349915cf15b0e4159c59b27d561bed3afbb50c3
Elias Alban <Alban001@users.noreply.github.com>
2024-06-25 16:32:01 -0400

    comm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts${cat}`);
+        const res = await axios.get(`/servidor/posts${cat}`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
