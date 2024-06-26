1f1a4ff5523bc09c5f3b277a4bd0959188ee383e
Elias Alban <Alban001@users.noreply.github.com>
2024-06-26 11:06:11 -0400

    info de usuario cambiado

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,7 +6,7 @@ const User = () => {
         <img src="upload/jose.jpeg" alt="" />
         <h1>José Cruz Falú</h1>
         <p>
-        Educador, escritor y motivador inspiracional, nos brinda reflexiones diarias que resultan muy refrescantes para todos. Las mismas vienen cargadas de mucha chispa, picardía, energía, erotismo y humor. Estas nos invitan a meditar acerca de los eventos que nos ocurren a todos diariamente. Al leer cada una de ellas te sentirás identificado, motivado y lleno de vitalidad para disfrutar  cada minuto de tu día desde una perspectiva única, nueva, diferente y vibrante.
+        Oxé Falú,  educador, escritor y motivador inspiracional puertorriqueño. Graduado de la Escuela de Comunicación Pública  de la Universidad de  Puerto Rico con una concentración en periodismo en 1982. Por 30 años ejerció como educador bilingüe en Texas. En sus escritos nos brinda reflexiones diarias que resultan muy refrescantes para todos. Las mismas vienen cargadas de mucha chispa, picardía, energía, erotismo y humor. Estas nos invitan a meditar acerca de los eventos que nos ocurren a todos diariamente. Al leer cada una de ellas te sentirás identificado, motivado y lleno de vitalidad para disfrutar  cada minuto de tu día desde una perspectiva única, nueva, diferente y vibrante dándole voz a una visión madura y atrevida a la realidad actual.
         </p>
     </div>
   )
