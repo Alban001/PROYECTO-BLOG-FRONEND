4e1872ab3c657a96783ca8a352158cc736a349f2
Elias Alban <Alban001@users.noreply.github.com>
2024-06-28 07:30:53 -0400

    post cambiado

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -22,7 +22,7 @@ const Write = () => {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      const res = await axios.post('/upload', formData);
+      const res = await axios.post('https://proyecto-bog-backend.onrender.com/upload', formData);
       return res.data;
     } catch (err) {
       console.log(err);
